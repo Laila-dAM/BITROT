19f9853f170848095890b7d418d0f01af3bbaecc
Laila de Araújo Mota <155311614+Laila-dAM@users.noreply.github.com>
2026-01-02 20:15:03 -0300

    Add global system header and application lifecycle

---
+++ Project 3/include/inventory_system.h
@@ -0,0 +1,10 @@
+#ifndef INVENTORY_SYSTEM_H
+#define INVENTORY_SYSTEM_H
+
+#define APP_NAME "Inventory Management System"
+#define APP_VERSION "0.1.0"
+
+int system_start(void);
+int system_shutdown(void);
+
+#endif
--- Project 3/src/main.c
+++ Project 3/src/main.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "inventory_system.h"
+
+int system_start(void)
+{
+    printf("%s\n", APP_NAME);
+    printf("Version %s\n", APP_VERSION);
+    return 0;
+}
+
+int system_shutdown(void)
+{
+    return 0;
+}
 
 int main(void)
 {
-    printf("Inventory Management System\n");
-    printf("Application starting...\n");
+    system_start();
+    system_shutdown();
     return EXIT_SUCCESS;
 }
