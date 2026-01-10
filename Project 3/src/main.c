d03037c75461554fa13b6270cc799381ab3f9f59
Laila-dAM <lailamota.238@gmail.com>
2026-01-10 17:42:29 -0300

    Build system operational with CMake and Ninja

--- Project 3/src/main.c
+++ Project 3/src/main.c
@@ -2,6 +2,16 @@
 #include <sqlite3.h>
 
 int main() {
-    printf("StockCore CLI initialized\n");
+    sqlite3 *db;
+    int rc = sqlite3_open("stockcore.db", &db);
+
+    if (rc) {
+        printf("Cannot open database\n");
+        return 1;
+    }
+
+    printf("StockCore CLI started successfully\n");
+
+    sqlite3_close(db);
     return 0;
 }
